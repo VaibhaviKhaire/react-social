de8a6f34790a6c89071b30cae095344391e37a49
Vaibhavi Khaire <vaibhavikhire81@gmail.com>
2024-07-01 15:46:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+import Home from "./components/pages/home/Home.jsx";
+
+function App() {
+  return <Home />
+}
+
+export default App;
---
+++ src/components/closeFriend/CloseFriend.jsx
@@ -0,0 +1,9 @@
+import "./closefriend.css";
+export default function CloseFriend({ user }) {
+    return (
+        <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
+            <span className="sidebarFriendname">{user.username}</span>
+        </li>
+    )
+} 
